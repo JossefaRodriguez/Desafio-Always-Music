402916a879482dfa335a0e7f112c9364504196a0
Jossefa <javierarodriguezvera@gmail.com>
2022-05-25 22:20:12 -0400

    Ultimo commit

--- consultarLosEstudiantes.js
+++ consultarLosEstudiantes.js
@@ -1,8 +1,19 @@
-const consultarLosEstudiantes = async (client) => {
-    await client.connect()
-    const res = await client.query("SELECT * FROM estudiantes;")
-    console.log(res.rows)
-    await client.end()
+const consultarLosEstudiantes = async (client, release, pool) => {
+    const SQLQuery = {
+        rowMode: 'array',
+        text: 'SELECT * FROM estudiantes',
+        name: 'all-students', // Prepared Statement
+    }
+    await client.query(SQLQuery, (errorConsulta, res) => {
+        if(errorConsulta) {
+            console.error('¡HUBO UN ERROR! Revise el siguiente código:', errorConsulta.code)
+            pool.end()
+        } else {
+            console.log(res.rows)
+            release()
+            pool.end()
+        }
+    })
 }
 
 module.exports = consultarLosEstudiantes
--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const { Client } = require('pg')
-const args = process.argv.slice(2)
+const { Pool } = require('pg')
+
 const consultarLosEstudiantes = require('./consultarLosEstudiantes')
 const agregarEstudiante = require ('./agregarEstudiante')
 const consultarEstudiante = require ('./consultarEstudiante')
 const editarEstudiante = require ('./editarEstudiante')
 const eliminarEstudiante = require ('./eliminarEstudiante')
 
+const args = process.argv.slice(2)
 const comandoEjecutar = args[0]
 
 const config = {
     user: 'postgres',
     host: 'localhost',
-    database: 'estudiantes_db',
     password: '1234',
+    database: 'estudiantes_db',
     port: 5432,
+    max: 20, // cantidad máxima de clientes conectados
+    idleTimeoutMillis: 5000, // cantidad de milisegundos en que un usuario puede estar inactivo
+    connectionTimeoutMillis: 2000, //cantidad de milisegundos de espera para que un nuevo usuario pueda conectarse
 }
 
-const client = new Client(config)
+const pool = new Pool(config)
 
 const ejecutarPrograma = async (comando) => {
-    if(comando === 'nuevo') {
-        agregarEstudiante(client)
-    } else if (comando === 'consulta') {
-        consultarLosEstudiantes(client)
-    } else if (comando === 'editar') {
-        editarEstudiante(client)
-    } else if (comando === 'rut') {
-        consultarEstudiante(client)
-    } else if (comando === 'eliminar') {
-        eliminarEstudiante(client)
-    } else {
-        console.log(error)
-    }
+    pool.connect((errorConexion, client, release) => { // errorConexion es por si hay error en la conexion
+        if(errorConexion) {
+            console.error('¡HUBO UN ERROR! Revise el siguiente código:', errorConexion.code)
+        } else {
+            if(comando === 'nuevo') {
+                agregarEstudiante(client, release, pool)
+            } else if (comando === 'consulta') {
+                consultarLosEstudiantes(client, release, pool)
+            } else if (comando === 'editar') {
+                editarEstudiante(client, release, pool)
+            } else if (comando === 'rut') {
+                consultarEstudiante(client, release, pool)
+            } else if (comando === 'eliminar') {
+                eliminarEstudiante(client, release, pool)
+            } else {
+                console.error("Error en la consulta")
+            }
+        }
+    })
 }
 
 ejecutarPrograma(comandoEjecutar)
